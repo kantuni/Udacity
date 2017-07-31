fc0b999c96c1d9cc8a4a5f41a38f52383c43421c
kantuni <henrikh.kantuni@gmail.com>
2017-07-31 17:54:34 +0400

    T02.02-Exercise-AddMenu

--- Developing Android Apps/GitHubQuery/app/src/main/java/com/example/githubquery/MainActivity.java
+++ Developing Android Apps/GitHubQuery/app/src/main/java/com/example/githubquery/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.githubquery;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -15,5 +18,23 @@ public class MainActivity extends Activity {
     EditText searchBoxEditText = findViewById(R.id.et_search_box);
     TextView urlDisplayTextView = findViewById(R.id.tv_url_display);
     TextView searchResultsTextView = findViewById(R.id.tv_github_search_results_json);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.main, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    int menuItemSelected = item.getItemId();
+
+    if (menuItemSelected == R.id.action_search) {
+      Toast.makeText(MainActivity.this, "Search is called", Toast.LENGTH_LONG).show();
+      return true;
+    }
+
+    return super.onOptionsItemSelected(item);
   }
 }
