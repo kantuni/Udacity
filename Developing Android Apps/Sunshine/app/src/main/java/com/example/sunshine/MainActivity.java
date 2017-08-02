d2d18e98f0bb346ccc6e518d33036844b10d8a53
kantuni <henrikh.kantuni@gmail.com>
2017-08-02 18:41:47 +0400

    S02.01-Exercise-Networking

---
+++ Developing Android Apps/Sunshine/app/src/main/java/com/example/sunshine/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.sunshine;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.TextView;
+import com.example.sunshine.data.SunshinePreferences;
+import com.example.sunshine.utilities.NetworkUtils;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class MainActivity extends Activity {
+
+  private TextView mWeatherTextView;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    mWeatherTextView = findViewById(R.id.tv_weather_data);
+    loadWeatherData();
+  }
+
+  private void loadWeatherData() {
+    String location = SunshinePreferences.getPreferredWeatherLocation(getApplicationContext());
+    URL weatherDataUrl = NetworkUtils.buildUrl(location);
+    new FetchWeatherTask().execute(weatherDataUrl);
+  }
+
+  public class FetchWeatherTask extends AsyncTask<URL, Void, String> {
+    @Override
+    protected String doInBackground(URL... urls) {
+      URL query = urls[0];
+      String weatherData = null;
+      try {
+        weatherData = NetworkUtils.getResponseFromHttpUrl(query);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      return weatherData;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+      if (s != null && !s.equals("")) {
+        mWeatherTextView.setText(s);
+      }
+    }
+  }
+}
