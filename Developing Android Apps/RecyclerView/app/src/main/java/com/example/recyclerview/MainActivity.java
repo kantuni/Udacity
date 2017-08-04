a801eee287530d2f74c87639a93133374f4b5364
kantuni <henrikh.kantuni@gmail.com>
2017-08-04 18:18:21 +0400

    T03.04-Exercise-WiringUpRecyclerView

--- Developing Android Apps/RecyclerView/app/src/main/java/com/example/recyclerview/MainActivity.java
+++ Developing Android Apps/RecyclerView/app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.recyclerview;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends Activity {
+
+  private static final int NUM_LIST_ITEMS = 100;
+  private GreenAdapter mAdapter;
+  private RecyclerView mNumbersList;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    mNumbersList = findViewById(R.id.rv_numbers);
+    LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+    mNumbersList.setLayoutManager(layoutManager);
+    mNumbersList.setHasFixedSize(true);
+    mAdapter = new GreenAdapter(NUM_LIST_ITEMS);
+    mNumbersList.setAdapter(mAdapter);
   }
 }
