b5cd415b3f717e9d87e4645e42eb52fe96328e74
kantuni <henrikh.kantuni@gmail.com>
2017-08-05 18:33:15 +0400

    T04b.01-Exercise-OpenWebpage

--- Developing Android Apps/Intents/app/src/main/java/com/example/intents/MainActivity.java
+++ Developing Android Apps/Intents/app/src/main/java/com/example/intents/MainActivity.java
@@ -1,35 +1,75 @@
 package com.example.intents;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
-
-  Button mBtn;
-  EditText mEditText;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+  }
 
-    mBtn = findViewById(R.id.button);
-    mEditText = findViewById(R.id.edit_text);
+  /**
+   * This method is called when the Open Website button is clicked. It will open the website
+   * specified by the URL represented by the variable urlAsString using implicit Intents.
+   *
+   * @param v Button that was clicked.
+   */
 
-    mBtn.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        Context context = MainActivity.this;
-        Class destinationActivity = ChildActivity.class;
-        Intent intent = new Intent(context, destinationActivity);
-        intent.putExtra(Intent.EXTRA_TEXT, mEditText.getText().toString());
-        MainActivity.this.startActivity(intent);
-      }
-    });
+  public void onClickOpenWebsiteButton(View v) {
+    String website = "https://udacity.com";
+    openWebsite(website);
+  }
+
+  /**
+   * This method is called when the Open Location in Map button is clicked. It will open the
+   * a map to the location represented by the variable addressString using implicit Intents.
+   *
+   * @param v Button that was clicked.
+   */
+
+  public void onClickOpenAddressButton(View v) {
+    Toast.makeText(this, "TODO: Open a map when this button is clicked", Toast.LENGTH_SHORT).show();
+  }
+
+  /**
+   * This method is called when the Share Text Content button is clicked. It will simply share
+   * the text contained within the String textThatYouWantToShare.
+   *
+   * @param v Button that was clicked.
+   */
+
+  public void onClickShareTextButton(View v) {
+    Toast.makeText(this, "TODO: Share text when this is clicked", Toast.LENGTH_LONG).show();
+  }
+
+  /**
+   * This is where you will create and fire off your own implicit Intent. Yours will be very
+   * similar to what I've done above. You can view a list of implicit Intents on the Common
+   * Intents page from the developer documentation.
+   *
+   * @param v Button that was clicked.
+   */
+
+  public void createYourOwn(View v) {
+    Toast.makeText(this,
+        "TODO: Create Your Own Implicit Intent",
+        Toast.LENGTH_SHORT)
+        .show();
+  }
+
+  private void openWebsite(String url) {
+    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+    PackageManager packageManager = MainActivity.this.getPackageManager();
+    if (intent.resolveActivity(packageManager) != null) {
+      startActivity(intent);
+    }
   }
 }
